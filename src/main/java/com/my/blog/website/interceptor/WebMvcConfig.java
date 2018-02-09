dba99dbfefeab3a24bc5fd5285085fed3b23eba5
zhongqing.lin <zhongqing.lin@zkteco.com>
2018-02-09 15:06:52 +0800

    修改部分源码

--- src/main/java/com/my/blog/website/interceptor/WebMvcConfig.java
+++ src/main/java/com/my/blog/website/interceptor/WebMvcConfig.java
@@ -1,34 +1,42 @@
 package com.my.blog.website.interceptor;
 
 
-import com.my.blog.website.utils.TaleUtils;
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.annotation.Resource;
+import com.my.blog.website.utils.TaleUtils;
 
 /**
- * 向mvc中添加自定义组件
- * Created by BlueT on 2017/3/9.
+ * 向mvc中添加自定义组件 Created by BlueT on 2017/3/9.
  */
 @Component
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
-    @Resource
-    private BaseInterceptor baseInterceptor;
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(baseInterceptor);
-    }
+  private static final Logger LOGGER = LoggerFactory.getLogger(WebMvcConfig.class);
 
-    /**
-     * 添加静态资源文件，外部可以直接访问地址
-     * @param registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations("file:"+ TaleUtils.getUplodFilePath()+"upload/");
-        super.addResourceHandlers(registry);
-    }
+  @Resource
+  private BaseInterceptor baseInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(baseInterceptor);
+  }
+
+  /**
+   * 添加静态资源文件，外部可以直接访问地址
+   * 
+   * @param registry
+   */
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    String uploadPath = "file:" + TaleUtils.getWebSiteRootPath() + "/upload/";
+    LOGGER.info("uploadPath: " + uploadPath);
+    registry.addResourceHandler("/upload/**").addResourceLocations(uploadPath);
+    super.addResourceHandlers(registry);
+  }
 }
